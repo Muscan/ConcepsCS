0f9f9c3323b90ec5dc1aaac6a7cb515a4eac2773
Mihai <kubikweg@gmail.com>
2022-03-28 12:38:16 +0300

    Add project files.

---
+++ Lists/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lists;
+
+SimpleList sl = new SimpleList();
+List<int> sourceList = new List<int> { 1, 2, 3 };
+List<int> destinationList = new List<int>();
+sl.CopyList(sourceList, destinationList);
+sl.Print(destinationList);
+
+
+//declare an array which has the same size of the source list. 
+int[] destinationArray = new int[sourceList.Count];
+sl.CopyListToAnArray(destinationList, destinationArray);
+sl.PrintArray(destinationArray);
+
---
+++ Lists/SimpleList.cs
@@ -0,0 +1,62 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class SimpleList
+    {
+        //declare objects of type List
+        private List<string> List { get; set; }
+        private List<int> CountNumbers { get; set; }
+        private List<int> CountNumbers1 { get; set; }   
+        private List<object> ListObjects { get; set; }
+       
+        public SimpleList()
+        {
+            //Instantiate objects
+            List = new List<string>();
+            CountNumbers = new List<int>();
+            CountNumbers1 = new List<int>();
+            ListObjects = new List<object>(); 
+        }
+        //Write a program in C# Sharp to copy the elements one list into another list
+
+        public void CopyList(List<int> sourceList, List<int> destinationList)
+        {
+            for (int i = 0; i < sourceList.Count; i++)
+            {//in order to auto increase the size of the destination list,.
+             //.Add must be called
+                destinationList.Add(sourceList[i]);
+
+            }
+        }
+
+        public void Print(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintArray(int[] array)
+        {
+            for (int i = 0; i <array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void CopyListToAnArray(List<int> list, int[] destinationArray)
+        {
+            list.CopyTo(destinationArray);
+        }
+        
+    }
+}
