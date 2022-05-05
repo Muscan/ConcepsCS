a85e6a9fe3078438dcf4da319d1bc65e955e4a37
Mihai <kubikweg@gmail.com>
2022-05-05 08:39:31 +0300

    Sort and trace

--- Lists/Sort.cs
+++ Lists/Sort.cs
@@ -32,37 +32,34 @@ namespace Lists
             Console.WriteLine($"Execution Time for direct sort: {watch.ElapsedMilliseconds} ms");
         }
         /*3,9,7,1
-         * first iteration
-         for (int i = 0; i < arr.Length-1; i++)
-         {
-                for (int j = i + 1; j < arr.Length; j++)       //j=i+1 
-                {
-                    if (arr[i] > arr[j]) //if 0(3) > 1(9)
-                    {
-                        temp = arr[i];   //-
-                        arr[i] = arr[j]; //-
-                        arr[j] = temp;   //-
-                    }  
-                }
-        }
-        
-                                                                                                           0      1      2     3
-          i   |  i < arr.Length-1|  j     | j < arr.Length| arr[i] > arr[j]  | temp| arr[i] |  arr[j]   |  3   |  9   |  7  |  1  |
-          0      (4-1)=3;0<3 T     (0+1)1   (1<4)T          arr[0] > arr[1]F    -      -         -         3      9      7     1
-          0            -            2        2<4 T          arr[0] > arr[2]F    -      -         -         3      9      7     1 
-          0            -            3        3<4 T          arr[0] > arr[3]T    3      1         3         1      9      7     3
-          0            -            4        4<4 F                 -            -      -         -         1      9      7     3
-          1          1<3T          (i+1)2    2<4 T          arr[1] > arr[2]T    9      7         9         1      7      9     3
-          1          -             j++ 3     3<4 T          arr[1] > arr[3]T    7      3         7         1      3      9     7
-          1          -             4         4<4 F                 -            -      -         -         1      3      9     7
-          2          2<3T          (2+1)3    3<4 T          arr[2] > arr[3]T    9      7         9         1      3      7     9
-          -           -            (3+1)4    4<4F                -              -      -         -         1      3      7     9
-          3          3<3F           -         -                  -              -      -         -         -      -       -    -
+        * first iteration
+        for (int i = 0; i < arr.Length-1; i++)
+        {
+               for (int j = i + 1; j < arr.Length; j++)       //j=i+1 
+               {
+                   if (arr[i] > arr[j]) //if 0(3) > 1(9)
+                   {
+                       temp = arr[i];   //-
+                       arr[i] = arr[j]; //-
+                       arr[j] = temp;   //-
+                   }  
+               }
+       }
 
-        
-         */
+                                                                                                          0      1      2     3
+         i   |  i < arr.Length-1|  j     | j < arr.Length| arr[i] > arr[j]  | temp| arr[i] |  arr[j]   |  3   |  9   |  7  |  1  |
+         0      (4-1)=3;0<3 T     (0+1)1   (1<4)T          arr[0] > arr[1]F    -      -         -         3      9      7     1
+         0            -            2        2<4 T          arr[0] > arr[2]F    -      -         -         3      9      7     1 
+         0            -            3        3<4 T          arr[0] > arr[3]T    3      1         3         1      9      7     3
+         0            -            4        4<4 F                 -            -      -         -         1      9      7     3
+         1          1<3T          (i+1)2    2<4 T          arr[1] > arr[2]T    9      7         9         1      7      9     3
+         1          -             j++ 3     3<4 T          arr[1] > arr[3]T    7      3         7         1      3      9     7
+         1          -             4         4<4 F                 -            -      -         -         1      3      9     7
+         2          2<3T          (2+1)3    3<4 T          arr[2] > arr[3]T    9      7         9         1      3      7     9
+         -           -            (3+1)4    4<4F                -              -      -         -         1      3      7     9
+         3          3<3F           -         -                  -              -      -         -         -      -       -    -
 
-      
+        */
 
 
         //Bubble sort method    
@@ -70,7 +67,7 @@ namespace Lists
         public int[] EfficientBubbleSort(int[] nr)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            //sort is stopped when the array is sorted and the sort stops because of the flag swapped. 
+
             bool swapped = false;
             int n = nr.Length;
             do
